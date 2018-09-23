c006faa41ddc92b829581360d7ae25a7a9209f4d
William George Cook <wgeorgecook@gmail.com>
2018-09-23 09:35:16 -0700

    Locationfilter component skeleton

---
+++ src/LocationFilter.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react-dom';
+
+class LocationFilter extends Component {
+    render() {
+        return (
+            <div>This is gonna be the location filter</div>
+        )
+    }
+}
+
+export default LocationFilter
