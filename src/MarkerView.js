20cf278fd8e2d62fae2e7b8ad48820dbf4cda248
William George Cook <wgeorgecook@gmail.com>
2018-09-23 09:36:48 -0700

    Marker view component skeleton

---
+++ src/MarkerView.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react-dom';
+
+class MarkerView extends Component {
+    render() {
+        return (
+            <div>This is gonna be the marker view!</div>
+        )
+    }
+}
+
+export default MarkerView
