bc6a6df2fa61738eae2c721fe6845bd98af4ae4d
William George Cook <wgeorgecook@gmail.com>
2018-09-23 09:33:38 -0700

    List view component skeleton

---
+++ src/ListView.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react-dom';
+
+class ListView extends Component {
+    render() {
+        return (
+            <div>This is gonna be the list view!</div>
+        )
+    }
+}
