6f2d3f98a567d2216aa927ba0795066ab79dba42
William George Cook <wgeorgecook@gmail.com>
2018-09-23 09:36:33 -0700

    Exporting MapView

---
+++ src/MapView.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react-dom';
+
+class MapView extends Component {
+    render() {
+        return (
+            <div>This is gonna be the map view!</div>
+        )
+    }
+}
+
+export default MapView
