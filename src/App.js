0deef6ab20da0bfdc8cbe5ecf2617b5e78a6d6c0
William George Cook <wgeorgecook@gmail.com>
2018-09-25 14:29:23 -0700

    Props for a default marker on the map

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import ListView from './ListView.js';
 import LocationFilter from './LocationFilter.js';
 import MapView from './MapView.js';
-import MarkerView from './MarkerView.js';
 import './App.css';
 
 class App extends Component {
@@ -15,8 +14,8 @@ class App extends Component {
           defaultLocation={{lat:28.417664 , lng: -81.581215}}
           defaultZoom={15}
           defaultHeight={'400px'}
+          defaultMarkers={{lat:28.417664 , lng: -81.581215}}
         />
-        <MarkerView />
       </div>
     );
   }
