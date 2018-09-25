a703a83a5652a68322b0b1627bc64d64c36ab195
William George Cook <wgeorgecook@gmail.com>
2018-09-25 14:23:16 -0700

    Customizing map view component from react-google-maps

--- src/MapView.js
+++ src/MapView.js
@@ -6,7 +6,7 @@ class MapView extends Component {
 
     MyMapComponent = withScriptjs(withGoogleMap((props) =>
         <GoogleMap
-        defaultZoom={8}
+        defaultZoom={this.props.defaultZoom}
         defaultCenter={this.props.defaultLocation}
         >
         {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
@@ -17,12 +17,13 @@ class MapView extends Component {
     render() {
 
         return (
-            <div id="map"> <this.MyMapComponent
-            isMarkerShown={false}
-            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `400px` }} />}
-            mapElement={<div style={{ height: `100%` }} />}
+            <div id="map">
+            <this.MyMapComponent
+                isMarkerShown={false}
+                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyABZJIOmyU36I209r096vcD_WY39JMxRPQ"
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: this.props.defaultHeight }} />}
+                mapElement={<div style={{ height: `100%` }} />}
             />
             </div>
         )
