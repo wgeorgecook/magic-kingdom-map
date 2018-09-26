081029191cc1437e9b136644dccef2ccea0db08b
William George Cook <wgeorgecook@gmail.com>
2018-09-25 19:36:42 -0700

    Filter box displays values and logs changes

--- src/LocationFilter.js
+++ src/LocationFilter.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 
 class LocationFilter extends Component {
+
+    rides = this.props.attractions.rides
+
+    selectNew(e){
+        console.log(e.target.value); // Name of the new attraction
+    }
+
     render() {
+
         return (
-            <div>This is gonna be the location filter</div>
+
+            <select id='filter' onChange={this.selectNew}>
+                {this.rides.map((ride,i) =>
+                    <option
+                        key={i}
+                        value={ride.name}
+                    > {ride.name} </option>
+                )}
+            </select>
+
         )
     }
 }
