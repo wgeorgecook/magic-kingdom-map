08be9fdd1a35aa946d294e6c8f66e1d553af30ee
William George Cook <wgeorgecook@gmail.com>
2018-09-26 21:56:39 -0700

    Renamed to FourSquareInfo because they have a supported js api

---
+++ src/FourSquareInfo.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+class FourSquareInfo extends Component {
+
+    state = {
+        clientID: "ujX3z_HJUDzuoDjBEBObkA",
+        apiKey: "bceSq_iUcRhYLkYCbfEggdd9x2dxfGpyOn5xiMjfsOkZ55ZfaO75amgDO18lDkOFlIgilK6JiSO9Z3ynY03SKnH8Idh4Xg3yCFpNPbsY6NhGKvDl3d08EWbRelmsW3Yx"
+
+    }
+
+    getReview(location) {
+        return null
+    }
+
+    render() {
+        return (
+            <div>FourSquareInfo info!</div>
+        )
+    }
+}
+
+export default FourSquareInfo
