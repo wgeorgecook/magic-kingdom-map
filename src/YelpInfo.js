d4f5ecfed56d541571b162de0759d7f8611c911d
William George Cook <wgeorgecook@gmail.com>
2018-09-26 21:18:16 -0700

    Yelp skeleton component

---
+++ src/YelpInfo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class YelpInfo extends Component {
+
+    render() {
+        return (
+            <div>Yelp info!</div>
+        )
+    }
+}
+
+export default YelpInfo
