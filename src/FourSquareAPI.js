d77de44251081c97b03b89f7c920194ac2980817
William George Cook <wgeorgecook@gmail.com>
2018-09-27 19:58:18 -0700

    Promises to resolve for data

--- src/FourSquareAPI.js
+++ src/FourSquareAPI.js
@@ -5,12 +5,11 @@ const tokens = {
     clientSecret: "WMJMPMLWRU4UNA1SBTLQ0KGRFTHZS44PTT5LMPPW5NSVILQX",
 };
 
-let searchResults;
 
-export const getDetails = (location, marker) => {
+export const getDetails = (location, marker) =>
     fetch(`https://api.foursquare.com/v2/venues/search?ll=${location.lat},${location.lng}&name=${marker.name}&client_id=${tokens.clientID}&client_secret=${tokens.clientSecret}&v=20180927&intent=match`)
         .then( (response) => response.json() )
-        .then( (jsonResponse) => (searchResults = jsonResponse) )
+        .then( (jsonResponse) => (jsonResponse) )
         .catch( (err) => console.log(err) )
-}
+
 
