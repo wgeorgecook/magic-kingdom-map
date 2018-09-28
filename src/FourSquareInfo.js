cfb8ec58c2094f8ce960052a4b2cd6bc89f2b0a1
William George Cook <wgeorgecook@gmail.com>
2018-09-27 19:13:23 -0700

    Minimizing this file's importance. May remove

--- src/FourSquareInfo.js
+++ src/FourSquareInfo.js
@@ -5,24 +5,30 @@ class FourSquareInfo extends Component {
 
     state = {
         clientID: "I2XVEDDAX1EILNB2YJ105QP3QGJDSSP4BRX3TMX2YF4S1MBW",
-        clientSecret: "WMJMPMLWRU4UNA1SBTLQ0KGRFTHZS44PTT5LMPPW5NSVILQX"
-
+        clientSecret: "WMJMPMLWRU4UNA1SBTLQ0KGRFTHZS44PTT5LMPPW5NSVILQX",
+        currentSearch: false
     }
 
     thisSearch;
 
-    getDetails(location) {
-        console.log(this.props.marker)
-        //console.log(`https://api.foursquare.com/v2/venues/search?ll=${location.lat},${location.lng}&client_id=${this.state.clientID}&client_secret=${this.state.clientSecret}&v=20180927`)
-        fetch(`https://api.foursquare.com/v2/venues/search?ll=${location.lat},${location.lng}&client_id=${this.state.clientID}&client_secret=${this.state.clientSecret}&v=20180927`)
+    getDetails(location, marker) {
+        // console.log(this.props.marker)
+        // console.log(`https://api.foursquare.com/v2/venues/search?ll=${location.lat},${location.lng}&name=${marker.name}&client_id=${this.state.clientID}&client_secret=${this.state.clientSecret}&v=20180927&intent=match`)
+        fetch(`https://api.foursquare.com/v2/venues/search?ll=${location.lat},${location.lng}&name=${marker.name}&client_id=${this.state.clientID}&client_secret=${this.state.clientSecret}&v=20180927&intent=match`)
             .then( (response) => response.json() )
-            .then( (jsonResponse) => (this.thisSearch = jsonResponse ) )
+            .then( (jsonResponse) => (this.setState( {currentSearch: jsonResponse} )) )
+            .then(this.props.onLoadDetails(this.state.currentSearch))
+            // .then(console.log(this.state.currentSearch))
             .catch( (err) => console.log(err) )
+
     }
 
     render() {
         return (
-            <div onClick={() => this.getDetails(this.props.location)}>{this.props.marker.name}</div>
+            <div className="thewindow">
+                <div onClick={() => this.getDetails(this.props.location, this.props.marker)}>{this.props.marker.name}</div>
+                <div className="4squareInfo">{this.props.dataToDisplay}</div>
+            </div>
         )
     }
 }
