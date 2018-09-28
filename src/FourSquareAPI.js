74dec6523a9347df08a369e86f743c7ceb58e219
William George Cook <wgeorgecook@gmail.com>
2018-09-27 19:13:00 -0700

    Making api calls separate from components

---
+++ src/FourSquareAPI.js
@@ -0,0 +1,16 @@
+
+
+const tokens = {
+    clientID: "I2XVEDDAX1EILNB2YJ105QP3QGJDSSP4BRX3TMX2YF4S1MBW",
+    clientSecret: "WMJMPMLWRU4UNA1SBTLQ0KGRFTHZS44PTT5LMPPW5NSVILQX",
+};
+
+let searchResults;
+
+export const getDetails = (location, marker) => {
+    fetch(`https://api.foursquare.com/v2/venues/search?ll=${location.lat},${location.lng}&name=${marker.name}&client_id=${this.state.clientID}&client_secret=${this.tokens.clientSecret}&v=20180927&intent=match`)
+        .then( (response) => response.json() )
+        .then( (jsonResponse) => (this.searchResults = jsonResponse) )
+        .catch( (err) => console.log(err) )
+}
+
