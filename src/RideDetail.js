d40909e029ac04fd92e72b1ef9bb83333be6ce29
William George Cook <wgeorgecook@gmail.com>
2018-09-29 12:05:20 -0700

    Details now include user photos

--- src/RideDetail.js
+++ src/RideDetail.js
@@ -8,6 +8,10 @@ class RideDetail extends Component {
         return (
             (this.props.apiData) &&
             <div className="rideItems">
+                <img
+                    alt={`${this.props.apiData.name}`}
+                    src={`${this.props.apiData.photos.groups[1].items[1].prefix}cap300${this.props.apiData.photos.groups[1].items[1].suffix}`}
+                />
                 <div>Name: {this.props.apiData.name}</div>
                 {(this.props.apiData.description && <div>Description: {this.props.apiData.description}</div> )}
                 <div>Rating: {this.props.apiData.rating}</div>
