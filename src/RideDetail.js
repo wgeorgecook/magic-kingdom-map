bf7d6bb072e8b15cea816c7569ee8a3969f1a61c
William George Cook <wgeorgecook@gmail.com>
2018-09-29 08:04:43 -0700

    Skeleton for more detailed view

---
+++ src/RideDetail.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class RideDetail extends Component {
+
+    render() {
+        return (
+            <div className="rideDetail">Some ride stuff here</div>
+        )
+    }
+}
+
+export default RideDetail
