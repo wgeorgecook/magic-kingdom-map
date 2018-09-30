7c5e29c98d9f858254cf84c4381e88c02c04cf03
William George Cook <wgeorgecook@gmail.com>
2018-09-29 18:47:14 -0700

    Attribution links in ride details

--- src/RideDetail.js
+++ src/RideDetail.js
@@ -11,11 +11,12 @@ class RideDetail extends Component {
                 <img
                     className="rideImg"
                     alt={`${this.props.apiData.name}`}
-                    src={`${this.props.apiData.photos.groups[1].items[1].prefix}cap300${this.props.apiData.photos.groups[1].items[1].suffix}`}
+                    src={`${this.props.apiData.photos.groups[1].items[0].prefix}cap300${this.props.apiData.photos.groups[1].items[0].suffix}`}
                 />
                 <div className="rideName">Name: {this.props.apiData.name}</div>
                 {(this.props.apiData.description && <div className="rideDesc">Description: {this.props.apiData.description}</div> )}
                 <div className="rideRating">Rating: {this.props.apiData.rating}</div>
+                <div className="attribution">See more about {this.props.apiData.name} on <a href={`http://foursquare.com/v/${this.props.apiData.id}?ref=I2XVEDDAX1EILNB2YJ105QP3QGJDSSP4BRX3TMX2YF4S1MBW`}>Foursquare!</a></div>
             </div> :
             <div className="noRide">Select a ride to see more!</div>
         )
