741e3b62abaedd508002fa8156195b9f24ebcf39
William George Cook <wgeorgecook@gmail.com>
2018-09-30 21:33:29 -0700

    Implement error boundary to catch load errors in map

--- src/MapView.js
+++ src/MapView.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {withScriptjs, withGoogleMap, GoogleMap, Marker} from 'react-google-maps';
+import ErrorBoundary from './errorBoundary.js'
 import InfoWindowView from './InfoWindowView.js';
 import * as FourAPI from './FourSquareAPI.js'
 
@@ -54,11 +55,12 @@ class MapView extends Component {
     }
 
 
+
+
     MyMapComponent = withScriptjs(withGoogleMap((props) =>
         <GoogleMap
             defaultZoom={this.props.defaultZoom}
             defaultCenter={this.props.defaultLocation}
-            onError={(error) => window.alert(`Google maps could not be loaded. Please try again: ${error}`)}
             >
             {props.isMarkerShown && this.props.markers.map( (marker, i) =>
                 <div key={i} className="markers-and-infobox">
@@ -83,17 +85,20 @@ class MapView extends Component {
   ))
 
 
+
     render() {
 
         return (
             <div id="map">
-            <this.MyMapComponent
-                isMarkerShown={true}
-                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyABZJIOmyU36I209r096vcD_WY39JMxRPQ"
-                loadingElement={<div style={{ height: `100%` }} />}
-                containerElement={<div style={{ height: this.props.defaultHeight }} />}
-                mapElement={<div style={{ height: `100%` }} />}
-            />
+                <ErrorBoundary>
+                    <this.MyMapComponent
+                        isMarkerShown={true}
+                        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyABZJIOmyU36I209r096vcD_WY39JMxRPQ"
+                        loadingElement={<div style={{ height: `100%` }} />}
+                        containerElement={<div style={{ height: this.props.defaultHeight }} />}
+                        mapElement={<div style={{ height: `100%` }} />}
+                    />
+                </ErrorBoundary>
             </div>
         )
     }
